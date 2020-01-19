88008010c4174bfb2f22a87a171abf115156fc3a
Vicente Ternicien <vicente.ternicien@gmail.com>
2020-01-19 17:37:29 -0300

    Testing functionality

--- Server/server.js
+++ Server/server.js
@@ -3,22 +3,70 @@
 const express = require('express');
 const app = express();
 const mongodb = require('mongodb');
+const PORT = 4000;
 
 const config = require('./db');
-const PORT = 4000;
-const client = mongodb.MongoClient;
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const cors = require ('cors');
+const bodyParser = require('body-parser')
 
-client.connect(config.DB, function(err, db) {
-    if(err) {
-        console.log('database is not connected')
-    }
-    else {
-        console.log('connected!!')
-    }
+const client = mongodb.MongoClient;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var newsSchema = new mongoose.Schema({
+    created_at:       Date,
+    title:            null | String,
+    url:              null | String,
+    author:           String,
+    points:           Number | null,
+    story_text:       null | String,
+    comment_text:     null | String,
+    num_comments:     Number | null,
+    story_id:         Number | null,
+    story_title:      null | String,
+    story_url:        null | String,
+    parent_id:        Number | null,
+    created_at_i:     Number,
+    objectID:         String,
+    isdeleted:        Number,
+  });
+
+var User = mongoose.model("user", newsSchema);
+
+//connect to DB
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true , useUnifiedTopology: true}, (err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  } else {
+    console.log('MongoDb conection OK');
+  }
 });
 
+//Load the data
+var ourRequest = new XMLHttpRequest()
+ourRequest.open('GET', 'https://hn.algolia.com/api/v1/search_by_date?query=nodejs')
+ourRequest.onload = dataload
+ourRequest.send();
+
+//Function to add an identifier, to know if the object as been deleted
+//It delete the elements that dont have a title
+function dataload(){
+    var newsdata = JSON.parse(ourRequest.responseText);
+    for (i = 0; i < newsdata.hits.length; i++) {
+        if (newsdata.hits[i].isdeleted == 1){}
+
+        else {newsdata.hits[i].isdeleted = 2;}
+      } 
+    // console.log(newsdata);
+    this.datostemp = newsdata;
+    return datostemp;
+}
+
 app.get('/', function(req, res) {
-    res.json({"hello": "world warl"});
+    res.send(datostemp.hits);
 });
 
 app.listen(PORT, function(){
