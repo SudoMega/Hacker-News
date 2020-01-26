499e2f55be0ccb1df0f9c3177f856245c0f9f387
Vicente Ternicien <vicente.ternicien@gmail.com>
2020-01-26 14:56:04 -0300

    new networking

--- Client/src/app/news/news.service.ts
+++ Client/src/app/news/news.service.ts
@@ -7,6 +7,8 @@ const urlnews = 'http://127.0.0.1:4000/news';
 const urlnews1 = 'http://192.168.99.100:4000/news';
 const urldelete = 'http://127.0.0.1:4000/delete';
 const urldelete1 = 'http://192.168.99.100:4000/delete/';
+const url = window.location.href;
+const ip = url.slice(0, -1);
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +18,11 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   getnewsdata(): Observable<NewsArray[]> {
-   return this.http.get<NewsArray[]>(urlnews);
+    console.log(url);
+    return this.http.get<NewsArray[]>(ip + ':4000/news');
 }
 
   deleteObject(id: string): Observable<ID> {
-    return this.http.get<ID>(urldelete + id);
+    return this.http.get<ID>(url + ':4000/delete/' + id);
   }
 }
